0548c53522679ec4c95325914f51de13c7152a71
David Reaver <me@davidreaver.com>
2023-04-13 08:28:40 -0700

    Add Array resp

--- src/resp.rs
+++ src/resp.rs
@@ -14,6 +14,10 @@ pub enum Message {
     /// Errors are similar to RESP Simple Strings, but the first character is a
     /// minus '-' character instead of a plus.
     Error(String),
+
+    /// Arrays are collections of RESP commands. Notably, arrays are used to
+    /// send commands from the client to the Redis server.
+    Array(Vec<Message>),
 }
 
 impl Message {
@@ -31,6 +35,15 @@ impl Message {
                 writer.write_all(b"-")?;
                 writer.write_all(s.as_bytes())?;
                 writer.write_all(b"\r\n")?;
+            }
+            Self::Array(msgs) => {
+                writer.write_all(b"*")?;
+                writer.write_all(msgs.len().to_string().as_bytes())?;
+                writer.write_all(b"\r\n")?;
+
+                for msg in msgs {
+                    msg.serialize_resp(writer)?;
+                }
             }
         }
 
@@ -52,6 +65,19 @@ impl Message {
         match first_line.chars().next() {
             Some('+') => Ok(Self::SimpleString(first_line[1..].to_string())),
             Some('-') => Ok(Self::Error(first_line[1..].to_string())),
+            Some('*') => {
+                let num_msgs = first_line[1..]
+                    .parse::<usize>()
+                    .wrap_err("invalid array length")?;
+                let mut msgs = Vec::with_capacity(num_msgs);
+                for i in 0..num_msgs {
+                    msgs.push(
+                        Self::parse_resp(reader)
+                            .wrap_err(eyre!("failed to parse array elem {i}"))?,
+                    );
+                }
+                Ok(Self::Array(msgs))
+            }
             Some(c) => Err(eyre!("invalid message start: {c}")),
             None => Err(eyre!("empty message")),
         }
@@ -62,29 +88,49 @@ impl Message {
 mod tests {
     use super::*;
 
-    #[test]
-    fn simple_serialize_to_resp() {
+    fn assert_message_round_trip(msg: &Message, expected: &[u8]) {
         let mut buf = Vec::new();
-        Message::SimpleString("OK".to_string())
-            .serialize_resp(&mut buf)
-            .unwrap();
-        assert_eq!(buf, b"+OK\r\n");
-
-        let mut buf = Vec::new();
-        Message::Error("ERROR my error".to_string())
-            .serialize_resp(&mut buf)
-            .unwrap();
-        assert_eq!(buf, b"-ERROR my error\r\n");
+        msg.serialize_resp(&mut buf).unwrap();
+        assert_eq!(buf, expected);
+        let msg2 = Message::parse_resp(&mut buf.as_slice()).unwrap();
+        assert_eq!(msg, &msg2);
     }
 
     #[test]
-    fn simple_parse_resp() {
-        let input = "+OK".to_string();
-        let msg = Message::parse_resp(&mut input.as_bytes()).unwrap();
-        assert_eq!(msg, Message::SimpleString("OK".to_string()));
+    fn simple_string_round_trip() {
+        assert_message_round_trip(&Message::SimpleString("OK".to_string()), b"+OK\r\n");
+    }
 
-        let input = "-ERROR my error".to_string();
-        let msg = Message::parse_resp(&mut input.as_bytes()).unwrap();
-        assert_eq!(msg, Message::Error("ERROR my error".to_string()));
+    #[test]
+    fn error_round_trip() {
+        assert_message_round_trip(
+            &Message::Error("ERROR my error".to_string()),
+            b"-ERROR my error\r\n",
+        );
+    }
+
+    #[test]
+    fn array_round_trip() {
+        assert_message_round_trip(&Message::Array(Vec::new()), b"*0\r\n");
+        assert_message_round_trip(
+            &Message::Array(vec![Message::SimpleString("OK".to_string())]),
+            b"*1\r\n+OK\r\n",
+        );
+        assert_message_round_trip(
+            &Message::Array(vec![
+                Message::SimpleString("OK".to_string()),
+                Message::SimpleString("blah".to_string()),
+            ]),
+            b"*2\r\n+OK\r\n+blah\r\n",
+        );
+
+        assert_message_round_trip(
+            &Message::Array(vec![
+                Message::Array(vec![Message::SimpleString("nested".to_string())]),
+                Message::SimpleString("OK".to_string()),
+                Message::SimpleString("blah".to_string()),
+            ]),
+            b"*3\r\n*1\r\n+nested\r\n+OK\r\n+blah\r\n",
+        );
     }
 }
