9d16c91b56cce407ee9874a49f1ad5f7bc4dd538
David Reaver <me@davidreaver.com>
2023-04-13 08:15:03 -0700

    Add error RESP type

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,23 +1,27 @@
 use std::io::{BufReader, Write};
 use std::net::TcpStream;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Context, Result};
 use redis_clone::resp::Message;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
     let mut stream = TcpStream::connect("127.0.0.1:6379")?;
+    let mut reader = BufReader::new(stream.try_clone().wrap_err("failed to clone stream")?);
 
-    println!("sending PING");
-    // TODO: Send ping using Message. I think we need to send
-    // "*1\r\n\$4\r\nPING\r\n" or "*1\r\n+PING\r\n"
-    stream.write_all(b"PING\r\n")?;
-    stream.flush()?;
+    let messages = vec!["PING", "nonsense"];
 
-    let mut reader = BufReader::new(stream);
-    let response = Message::parse_resp(&mut reader)?;
-    println!("Response: {response:?}");
+    for message in messages {
+        // TODO: Send ping using Message. I think we need to send
+        // "*1\r\n\$4\r\nPING\r\n" or "*1\r\n+PING\r\n"
+        println!("sending {}", message);
+        stream.write_all(message.as_bytes())?;
+        stream.write_all(b"\r\n")?;
+        stream.flush()?;
+        let response = Message::parse_resp(&mut reader)?;
+        println!("Response: {response:?}");
+    }
 
     Ok(())
 }
--- src/resp.rs
+++ src/resp.rs
@@ -10,6 +10,10 @@ pub enum Message {
     /// Simple Strings are used to transmit non binary-safe strings with minimal
     /// overhead. They cannot contain a CR or LF character.
     SimpleString(String),
+
+    /// Errors are similar to RESP Simple Strings, but the first character is a
+    /// minus '-' character instead of a plus.
+    Error(String),
 }
 
 impl Message {
@@ -20,6 +24,11 @@ impl Message {
         match self {
             Self::SimpleString(s) => {
                 writer.write_all(b"+")?;
+                writer.write_all(s.as_bytes())?;
+                writer.write_all(b"\r\n")?;
+            }
+            Self::Error(s) => {
+                writer.write_all(b"-")?;
                 writer.write_all(s.as_bytes())?;
                 writer.write_all(b"\r\n")?;
             }
@@ -42,6 +51,7 @@ impl Message {
 
         match first_line.chars().next() {
             Some('+') => Ok(Self::SimpleString(first_line[1..].to_string())),
+            Some('-') => Ok(Self::Error(first_line[1..].to_string())),
             Some(c) => Err(eyre!("invalid message start: {c}")),
             None => Err(eyre!("empty message")),
         }
@@ -59,6 +69,12 @@ mod tests {
             .serialize_resp(&mut buf)
             .unwrap();
         assert_eq!(buf, b"+OK\r\n");
+
+        let mut buf = Vec::new();
+        Message::Error("ERROR my error".to_string())
+            .serialize_resp(&mut buf)
+            .unwrap();
+        assert_eq!(buf, b"-ERROR my error\r\n");
     }
 
     #[test]
@@ -66,5 +82,9 @@ mod tests {
         let input = "+OK".to_string();
         let msg = Message::parse_resp(&mut input.as_bytes()).unwrap();
         assert_eq!(msg, Message::SimpleString("OK".to_string()));
+
+        let input = "-ERROR my error".to_string();
+        let msg = Message::parse_resp(&mut input.as_bytes()).unwrap();
+        assert_eq!(msg, Message::Error("ERROR my error".to_string()));
     }
 }
