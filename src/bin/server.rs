64cb759bcce8c50b77ee42e03a9f34ccbd1ed03a
David Reaver <me@davidreaver.com>
2023-04-14 09:32:43 -0700

    Move server logic into server.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,97 +1,11 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::net::TcpListener;
-use std::thread;
-
 use color_eyre::eyre::Result;
 
-use redis_clone::command::{Command, CommandResponse};
-use redis_clone::resp::Message;
 use redis_clone::server::Server;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
-    println!("Listening on {}", listener.local_addr()?);
-
-    loop {
-        // Wait for a client to connect.
-        let (mut stream, addr) = listener.accept()?;
-        println!("connection received from {}", addr);
-
-        // Spawn a thread to handle this client.
-        thread::spawn(move || {
-            let mut write_stream = stream.try_clone().expect("failed to clone stream");
-            let mut writer = BufWriter::new(&mut write_stream);
-            let mut reader = BufReader::new(&mut stream);
-
-            if let Err(e) = client_loop(&mut reader, &mut writer) {
-                eprintln!("error in client thread: {}", e);
-            }
-            println!("connection closed for addr {addr}");
-        });
-    }
-}
-
-fn client_loop<R, W>(reader: &mut R, writer: &mut BufWriter<&mut W>) -> Result<()>
-where
-    R: BufRead,
-    W: Write,
-{
-    // TODO: Don't have a single server per thread. Have threads send
-    // commands to server.
-    let mut server = Server::new();
-
-    while let Some(response) = process_next_message(&mut server, reader) {
-        let response = response.to_resp();
-
-        println!("sending response: {:?}", response);
-        response
-            .serialize_resp(writer)
-            .expect("error in client thread");
-        writer.flush()?;
-    }
+    Server::start("127.0.0.1:6379")?;
 
     Ok(())
-}
-
-fn process_next_message<R>(server: &mut Server, reader: &mut R) -> Option<CommandResponse>
-where
-    R: BufRead,
-{
-    let message = match Message::parse_resp(reader) {
-        Ok(Some(m)) => m,
-        Ok(None) => {
-            return None;
-        }
-        Err(e) => {
-            return Some(CommandResponse::Error(format!(
-                "error parsing message: {}",
-                e
-            )));
-        }
-    };
-    println!("received message: {:?}", message);
-
-    let command = match Command::parse_resp(&message) {
-        Ok(c) => c,
-        Err(e) => {
-            return Some(CommandResponse::Error(format!("error parsing RESP: {}", e)));
-        }
-    };
-    println!("parsed command: {:?}", command);
-
-    let response = match server.process_command(command) {
-        Ok(r) => r,
-        Err(e) => {
-            return Some(CommandResponse::Error(format!(
-                "error processing command: {}",
-                e
-            )));
-        }
-    };
-
-    println!("SERVER STATE: {:?}", server);
-
-    Some(response)
 }
--- src/server.rs
+++ src/server.rs
@@ -1,38 +1,128 @@
 //! Core server functionality for redis-clone.
 
 use std::collections::HashMap;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::net::TcpListener;
+use std::thread;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result, WrapErr};
 
 use crate::command::{Command, CommandResponse, Get, Set};
+use crate::resp::Message;
 use crate::string::RedisString;
 
-/// A `Server` is the core of the redis-clone server. It contains the
-/// key-value store and the logic for handling commands.
+/// A `Server` is a redis-clone server.
 #[derive(Debug)]
-pub struct Server {
+pub struct Server {}
+
+impl Server {
+    pub fn start<A>(addr: A) -> Result<Self>
+    where
+        A: std::net::ToSocketAddrs,
+    {
+        let listener = TcpListener::bind(addr).wrap_err_with(|| eyre!("failed to start server"))?;
+
+        println!("Listening on {}", listener.local_addr()?);
+
+        loop {
+            // Wait for a client to connect.
+            let (mut stream, addr) = listener.accept()?;
+            println!("connection received from {addr}");
+
+            // Spawn a thread to handle this client.
+            thread::spawn(move || {
+                let mut write_stream = stream.try_clone().expect("failed to clone stream");
+                let mut writer = BufWriter::new(&mut write_stream);
+                let mut reader = BufReader::new(&mut stream);
+
+                if let Err(e) = client_loop(&mut reader, &mut writer) {
+                    eprintln!("error in client thread: {e}");
+                }
+                println!("connection closed for addr {addr}");
+            });
+        }
+    }
+}
+
+fn client_loop<R, W>(reader: &mut R, writer: &mut BufWriter<&mut W>) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
+    // TODO: Don't have a single server core per thread. Have threads send
+    // commands to server.
+    let mut core = ServerCore::new();
+
+    while let Some(response) = process_next_message(&mut core, reader) {
+        let response = response.to_resp();
+
+        println!("sending response: {response:?}");
+        response
+            .serialize_resp(writer)
+            .expect("error in client thread");
+        writer.flush()?;
+    }
+
+    Ok(())
+}
+
+fn process_next_message<R>(core: &mut ServerCore, reader: &mut R) -> Option<CommandResponse>
+where
+    R: BufRead,
+{
+    let message = match Message::parse_resp(reader) {
+        Ok(Some(m)) => m,
+        Ok(None) => {
+            return None;
+        }
+        Err(e) => {
+            return Some(CommandResponse::Error(format!(
+                "error parsing message: {e}"
+            )));
+        }
+    };
+    println!("received message: {message:?}");
+
+    let command = match Command::parse_resp(&message) {
+        Ok(c) => c,
+        Err(e) => {
+            return Some(CommandResponse::Error(format!("error parsing RESP: {e}")));
+        }
+    };
+    println!("parsed command: {command:?}");
+
+    let response = core.process_command(command);
+    println!("SERVER STATE: {core:?}");
+
+    Some(response)
+}
+
+/// A `ServerCore` is primary command processor of the redis-clone server. It
+/// contains the key-value store and the logic for handling commands.
+#[derive(Debug)]
+struct ServerCore {
     key_value: HashMap<RedisString, RedisString>,
 }
 
-impl Server {
+impl ServerCore {
     pub fn new() -> Self {
         Self {
             key_value: HashMap::new(),
         }
     }
 
-    pub fn process_command(&mut self, command: Command) -> Result<CommandResponse> {
+    pub fn process_command(&mut self, command: Command) -> CommandResponse {
         match command {
-            Command::Ping => Ok(CommandResponse::Pong),
+            Command::Ping => CommandResponse::Pong,
             Command::Get(Get { key }) => {
                 let value = self.key_value.get(&key);
-                Ok(CommandResponse::BulkString(value.cloned()))
+                CommandResponse::BulkString(value.cloned())
             }
             Command::Set(Set { key, value }) => {
                 self.key_value.insert(key, value);
-                Ok(CommandResponse::Ok)
+                CommandResponse::Ok
             }
-            Command::RawCommand(c) => Ok(CommandResponse::Error(format!("unknown command: {c:?}"))),
+            Command::RawCommand(c) => CommandResponse::Error(format!("unknown command: {c:?}")),
         }
     }
 }
@@ -43,26 +133,26 @@ mod tests {
 
     #[test]
     fn test_ping() {
-        let mut server = Server::new();
-        let response = server.process_command(Command::Ping).unwrap();
+        let mut core = ServerCore::new();
+        let response = core.process_command(Command::Ping);
         assert_eq!(response, CommandResponse::Pong);
     }
 
     #[test]
     fn test_set_get() {
-        let mut server = Server::new();
+        let mut core = ServerCore::new();
 
         let set_command = Command::Set(Set {
             key: RedisString::from("key"),
             value: RedisString::from("value"),
         });
-        let response = server.process_command(set_command).unwrap();
+        let response = core.process_command(set_command);
         assert_eq!(response, CommandResponse::Ok);
 
         let get_command = Command::Get(Get {
             key: RedisString::from("key"),
         });
-        let response = server.process_command(get_command).unwrap();
+        let response = core.process_command(get_command);
         assert_eq!(
             response,
             CommandResponse::BulkString(Some(RedisString::from("value")))
